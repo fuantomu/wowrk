b559f89c51419bd8d62685adf54148ef4165e08c
fuantomu <fuantomuw@gmail.com>
2018-07-14 21:28:48 +0200

    Added basics of stats calculation from lobby > game (armor working for now) Worked a bit more on the tooltip (looks 80-90% fine)

--- game/gameView/bossAI/boss1.lua
+++ game/gameView/bossAI/boss1.lua
@@ -1,5 +1,5 @@
 local turnOrder = {}
-local stats = {id=1,name="The Boss",level=10,hpmax=10000,hpcurr=10000,resmax=0,rescurr=0,str=1,agi=1,sta=1000,int=50,spr=50,armor=41,icon="hws010"}
+local stats = {id=1,name="The Boss",level=10,hpmax=100,hpcurr=100,resmax=0,rescurr=0,str=1,agi=1,sta=1000,int=50,spr=50,armor=41,icon="hws010"}
 local abilities = 	{
 					{id=1,name="Punch",type="Melee",element="PHY",targets="RANDOM:1:{2,4}",elemtype="NAT",attr="STR", mult=100, cast=5.0},
 					{id=2,name="Kick",type="Melee",element="PHY",targets="ALL",elemtype="NAT",attr="AGI", mult=200, cast=4.5},
